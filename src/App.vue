a7c0d5ba34621d172dc65399e603b4a1cdd0d1b2
MaxAvila <maxvilam@hotmail.con>
2022-03-10 02:55:53 -0300

    ok-1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="style">
+    <h1 class="display-1">Crea una nueva tarea</h1>
+    <h3>Tarea</h3>
+    <input v-model="nombre" type="text" />
+    <button @click="Guardar" class="btn btn-info mx-1">Crear</button>
+    <h2>Lista</h2>
+    <ul class="centrarlista" v-for="(resultado, i) of arrayTarea" :key="i">
+      <li class="justify-content-center">{{ 1 + i }}: {{ resultado.nombre }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      arrayTarea: [],
+      nombre: "",
+    };
+  },
+  methods: {
+    Guardar() {
+      if (!this.nombre) return;
+      const unatarea = { nombre: this.nombre };
+      this.arrayTarea.push(unatarea);
+      console.log(this.arrayTarea);
+      this.nombre = "";
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
+#style {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.centrarlista{
+  display: flex;
+  justify-content: center;
 }
 </style>
